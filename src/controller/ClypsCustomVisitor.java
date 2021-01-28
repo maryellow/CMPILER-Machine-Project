148855f3a6ce0fa50e44d9119f94ba56563a2659
Secade <47266084+Secade@users.noreply.github.com>
2021-01-28 21:37:54 +0800

    Fixed Semantic and variable implementation

--- src/controller/ClypsCustomVisitor.java
+++ src/controller/ClypsCustomVisitor.java
@@ -3,6 +3,7 @@ package controller;
 import ErrorCheckers.TypeChecking;
 import antlr.ClypsBaseVisitor;
 import antlr.ClypsParser;
+import com.udojava.evalex.Expression;
 import items.ClypsValue;
 import sun.awt.Symbol;
 
@@ -16,9 +17,43 @@ public class ClypsCustomVisitor extends ClypsBaseVisitor<ClypsValue> {
 //        System.out.println(ctx.getChild(0).getChild(0).getText());
 //        System.out.println(ctx.getChild(0).getChild(1).getChild(0).getChild(0).getText());
 //        System.out.println(ctx.getChild(0).getChild(1).getChild(0).getChild(2).getText());
-        if (SymbolTableManager.searchVariableInLocalIterative(ctx.getChild(0).getChild(1).getChild(0).getChild(0).getText(),SymbolTableManager.getInstance().getActiveLocalScope().getParent())==null){
+        if (SymbolTableManager.searchVariableInLocalIterative(ctx.getChild(0).getChild(1).getChild(0).getChild(0).getText(),SymbolTableManager.getInstance().getActiveLocalScope())==null&&
+                SymbolTableManager.searchVariableInLocalIterative(ctx.getChild(0).getChild(1).getChild(0).getChild(0).getText(),SymbolTableManager.getInstance().getActiveLocalScope().getParent())==null){
             System.out.println("VAR NOT FOUND");
-            SymbolTableManager.getInstance().getActiveLocalScope().addInitializedVariableFromKeywords(ctx.getChild(0).getChild(0).getText(),ctx.getChild(0).getChild(1).getChild(0).getChild(0).getText(),ctx.getChild(0).getChild(1).getChild(0).getChild(2).getText());
+            boolean test1 = ctx.getChild(0).getChild(1).getChild(0).getChild(2).getText().contains("\"");
+            boolean test2 = ctx.getChild(0).getChild(1).getChild(0).getChild(2).getText().contains("'");
+
+            System.out.println(test2);
+            System.out.println(ctx.getChild(0).getChild(0).getText());
+            System.out.println(ClypsValue.translateType(ctx.getChild(0).getChild(0).getText()));
+            if (ClypsValue.translateType(ctx.getChild(0).getChild(0).getText())!= ClypsValue.PrimitiveType.STRING&&!test1&&!test2){
+                boolean test = new Expression(ctx.getChild(0).getChild(1).getChild(0).getChild(2).getText()).isBoolean();
+                if (!test&&ctx.getChild(0).getChild(1).getChild(0).getChild(2).getText().contains("true")||ctx.getChild(0).getChild(1).getChild(0).getChild(2).getText().contains("false"))
+                    test=true;
+                System.out.println("====");
+                System.out.println(test);
+                System.out.println(ctx.getChild(0).getChild(1).getChild(0).getChild(2).getText());
+                if (ClypsValue.translateType(ctx.getChild(0).getChild(0).getText())== ClypsValue.PrimitiveType.BOOLEAN&&test){
+                    System.out.println("IS BOOLEAN");
+                    SymbolTableManager.getInstance().getActiveLocalScope().addInitializedVariableFromKeywords(ctx.getChild(0).getChild(0).getText(),ctx.getChild(0).getChild(1).getChild(0).getChild(0).getText(),ctx.getChild(0).getChild(1).getChild(0).getChild(2).getText());
+                } else if (ClypsValue.translateType(ctx.getChild(0).getChild(0).getText())!= ClypsValue.PrimitiveType.BOOLEAN&&(test||ctx.getChild(0).getChild(1).getChild(0).getChild(2).getText().contains("true")||ctx.getChild(0).getChild(1).getChild(0).getChild(2).getText().contains("false"))){
+                    System.out.println("NOT BOOLEAN");
+                    editor.addCustomError("TYPE MISMATCH",ctx.start.getLine());
+                }else if (ClypsValue.translateType(ctx.getChild(0).getChild(0).getText())!= ClypsValue.PrimitiveType.BOOLEAN&&!test){
+                    System.out.println("IS DECIMAL");
+                    SymbolTableManager.getInstance().getActiveLocalScope().addInitializedVariableFromKeywords(ctx.getChild(0).getChild(0).getText(),ctx.getChild(0).getChild(1).getChild(0).getChild(0).getText(),ctx.getChild(0).getChild(1).getChild(0).getChild(2).getText());
+
+                }
+            }else if (ClypsValue.translateType(ctx.getChild(0).getChild(0).getText())== ClypsValue.PrimitiveType.STRING&&test1){
+                SymbolTableManager.getInstance().getActiveLocalScope().addEmptyVariableFromKeywords(ctx.getChild(0).getChild(0).getText(),ctx.getChild(0).getChild(1).getChild(0).getChild(0).getText());
+                SymbolTableManager.getInstance().getActiveLocalScope().searchVariableIncludingLocal(ctx.getChild(0).getChild(1).getChild(0).getChild(0).getText()).setSCValue(ctx.getChild(0).getChild(1).getChild(0).getChild(2).getText());
+            }else if(ClypsValue.translateType(ctx.getChild(0).getChild(0).getText())== ClypsValue.PrimitiveType.CHAR&&test2){
+                SymbolTableManager.getInstance().getActiveLocalScope().addEmptyVariableFromKeywords(ctx.getChild(0).getChild(0).getText(),ctx.getChild(0).getChild(1).getChild(0).getChild(0).getText());
+                SymbolTableManager.getInstance().getActiveLocalScope().searchVariableIncludingLocal(ctx.getChild(0).getChild(1).getChild(0).getChild(0).getText()).setSCValue(ctx.getChild(0).getChild(1).getChild(0).getChild(2).getText());
+
+            }else {
+                editor.addCustomError("TYPE MISMATCH",ctx.start.getLine());
+            }
 
         }else {
             editor.addCustomError("DUPLICATE VAR DETECTED",ctx.start.getLine());
--- src/items/ClypsValue.java
+++ src/items/ClypsValue.java
@@ -58,6 +58,7 @@ public class ClypsValue {
             this.type = primitiveType;
             if (this.type==PrimitiveType.INT||this.type==PrimitiveType.FLOAT||this.type==PrimitiveType.DOUBLE||this.type==PrimitiveType.BOOLEAN)
                 tryEvaluate(value.toString());
+
         }else{
             System.out.println("ERROR");
             editor.addError("Type Mismatch");
@@ -67,6 +68,14 @@ public class ClypsValue {
 
     public void setValue(String value){
         tryEvaluate(value);
+    }
+
+    public void setSCValue(String value){
+        if (value.contains("\""))
+            value=value.replaceAll("\"","");
+        else
+            value=value.replaceAll("'","");
+        this.value=value;
     }
 
     public void setType(PrimitiveType primitiveType){
@@ -91,8 +100,13 @@ public class ClypsValue {
 
     private void tryEvaluate(String value){
         System.out.println(value);
+        System.out.println(this.getPrimitiveType());
 
-        this.value = new Expression(value).eval().toPlainString();
+        if (this.getPrimitiveType()!=PrimitiveType.STRING&&this.getPrimitiveType()!=PrimitiveType.CHAR)
+            this.value = new Expression(value).eval().toPlainString();
+        else
+            this.value=value;
+
 
         System.out.println("EVAL: "+this.value);
     }
@@ -164,7 +178,7 @@ public class ClypsValue {
         else if(primitiveTypeString.contains(PrimitiveType.INT.toString().toLowerCase(Locale.ROOT))) {
             primitiveType = PrimitiveType.INT;
         }
-        else if(primitiveTypeString.contains(PrimitiveType.STRING.toString().toLowerCase(Locale.ROOT))) {
+        else if(primitiveTypeString.toLowerCase(Locale.ROOT).contains(PrimitiveType.STRING.toString().toLowerCase(Locale.ROOT))) {
             primitiveType = PrimitiveType.STRING;
         }
 
@@ -179,7 +193,7 @@ public class ClypsValue {
         if(primitiveTypeString.contains(PrimitiveType.BOOLEAN.toString())) {
             primitiveType = PrimitiveType.BOOLEAN;
         }
-        else if(primitiveTypeString.contains(PrimitiveType.CHAR.toString())) {
+        else if(primitiveTypeString.contains(PrimitiveType.CHAR.toString().toLowerCase(Locale.ROOT))) {
             primitiveType = PrimitiveType.CHAR;
         }
         else if(primitiveTypeString.contains(PrimitiveType.DOUBLE.toString())) {
@@ -191,7 +205,7 @@ public class ClypsValue {
         else if(primitiveTypeString.contains(PrimitiveType.INT.toString())) {
             primitiveType = PrimitiveType.INT;
         }
-        else if(primitiveTypeString.contains(PrimitiveType.STRING.toString())) {
+        else if(primitiveTypeString.toLowerCase(Locale.ROOT).contains(PrimitiveType.STRING.toString().toLowerCase(Locale.ROOT))) {
             primitiveType = PrimitiveType.STRING;
         }
 
